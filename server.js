198ceee1849d367968da13834eb0a79cf7033efb
roshini <jeevanaroshini@gmail.com>
2025-05-28 09:39:23 +0530

    reset changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const express = require("express");
-const serverless = require("serverless-http");
+// const serverless = require("serverless-http");
 const userroute = require("./routes/users/Userroute.js");
 const postroute = require("./routes/posts/Postroute.js");
 const commentroute = require("./routes/comments/Commentroute.js");
@@ -13,7 +13,7 @@ const methodOverride = require("method-override")
 
 
 
-require("./config/dbConnect");
+require("./config/dbConnect.js");
 const app = express();
 
 app.set('view engine','ejs')
@@ -41,8 +41,8 @@ app.use('/api/v1/',commentroute)
 
 //Error handler middlewares
 //listen server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
 
 // Export as serverless function
-module.exports = serverless(app);
+// module.exports = serverless(app);
